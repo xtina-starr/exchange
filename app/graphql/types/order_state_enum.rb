ef0728a2247139d85ac4256231ad64fb3ecfa8dd
ashkan <ashkan@artsymail.com>
2018-06-21 13:30:26 -0400

    Switch to Order::<> for enum

--- app/graphql/types/order_state_enum.rb
+++ app/graphql/types/order_state_enum.rb
@@ -1,7 +1,7 @@
 class Types::OrderStateEnum < Types::BaseEnum
-  value 'PENDING', 'order is still pending submission by collector', value: 'pending'
-  value 'SUBMITTED', 'order is submitted by collector', value: 'submitted'
-  value 'APPROVED', 'order is approved by partner', value: 'approved'
-  value 'REJECTED', 'order is rejected by partner', value: 'rejected'
-  value 'FINALIZED', 'order is finalized by partner', value: 'finalized'
+  value 'PENDING', 'order is still pending submission by collector', value: Order::PENDING
+  value 'SUBMITTED', 'order is submitted by collector', value: Order::SUBMITTED
+  value 'APPROVED', 'order is approved by partner', value: Order::APPROVED
+  value 'REJECTED', 'order is rejected by partner', value: Order::REJECTED
+  value 'FINALIZED', 'order is finalized by partner', value: Order::FINALIZED
 end
