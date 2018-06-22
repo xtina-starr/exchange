767c7406a4bd12e152bcdce22168b7dafc8f9538
ashkan <ashkan@artsymail.com>
2018-06-22 14:21:40 -0400

    Fix link in to readme

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,5 +3,5 @@ import {danger, warn} from "danger"
 const addedOrModified = danger.git.modified_files.concat(danger.git.created_files)
 const fileInGraphQLFolder = addedOrModified.find(f => f.startsWith("app/graphql"))
 if (fileInGraphQLFolder) {
-  warn("If you want these changes to be reflected in Metaphysics, you will need to [update the stored schema](https://github.com/artsy/exchange#did-you-change-graphql-schema?).")
+  warn("If you want these changes to be reflected in Metaphysics, you will need to [update the stored schema](https://github.com/artsy/exchange#did-you-change-graphql-schema).")
 }
