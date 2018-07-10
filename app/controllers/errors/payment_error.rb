9122d02e09e5e0c90ea6930419f30738116d5010
Will Doenlen <wdoenlen@gmail.com>
2018-07-10 16:55:53 -0400

    Add PaymentError class and check that credit card id isn't blank and that destination account id matches partner's account id

---
+++ app/controllers/errors/payment_error.rb
@@ -0,0 +1,4 @@
+module Errors
+  class PaymentError < ApplicationError
+  end
+end
--- app/services/payment_service.rb
+++ app/services/payment_service.rb
@@ -1,9 +1,10 @@
 module PaymentService
-  def self.authorize_charge(order)
+  def self.authorize_charge(order, amount=order.items_total_cents)
+    validate_payment_info(order)
     charge = Stripe::Charge.create(
-      amount: order.items_total_cents,
+      amount: amount,
       currency: 'usd',
-      description: 'Example charge',
+      description: 'Artsy',
       source: order.credit_card_id,
       destination: order.destination_account_id,
       capture: false
@@ -11,8 +12,13 @@ module PaymentService
     TransactionService.create!(order, charge)
   end
 
-  def self.validate_charge
-    #TODO: Check for valid partner account ID and credit card ID
+  def self.validate_payment_info(order)
+    raise Errors::PaymentError, 'Invalid credit card id' if order.credit_card_id.blank?
+    raise Errors::PaymentError, 'Invalid destination account id' unless valid_destination_account?(order.partner_id, order.destination_account_id)
   end
 
+  def self.valid_destination_account?(partner_id, destination_account_id)
+    merchant_account_ids = Adapters::GravityV1.request("/merchant_accounts?partner_id=#{partner_id}")
+    merchant_account_ids.pluck(:external_id).include? destination_account_id
+  end
 end
