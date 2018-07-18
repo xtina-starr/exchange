74729c5a7be2027ee7a76c654f424ffccec6665a
Will Doenlen <wdoenlen@gmail.com>
2018-07-18 17:42:52 -0400

    Fetch Stripe credit card ID from Gravity on order submit; modify PaymentService.authorize_charge to take in keyword arguments

--- app/services/order_submit_service.rb
+++ app/services/order_submit_service.rb
@@ -4,9 +4,21 @@ module OrderSubmitService
     Order.transaction do
       # verify price change?
       order.submit!
+
       merchant_account = get_merchant_account(order)
       raise Errors::OrderError, 'Partner does not have merchant account' if merchant_account.nil?
-      charge = PaymentService.authorize_charge(order.credit_card_id, merchant_account[:external_id], order.buyer_total_cents, order.currency_code)
+
+      credit_card = get_credit_card(order)
+      raise Errors::OrderError, 'User does not have stored credit card' if credit_card.blank?
+
+      charge_params = {
+        source_id: credit_card[:external_id],
+        customer_id: credit_card[:customer_account][:external_id],
+        destination_id: merchant_account[:external_id],
+        amount: order.buyer_total_cents,
+        currency_code: order.currency_code
+      }
+      charge = PaymentService.authorize_charge(charge_params)
       TransactionService.create_success!(order, charge)
       order.save!
     end
@@ -18,6 +30,10 @@ module OrderSubmitService
 
   def self.get_merchant_account(order)
     Adapters::GravityV1.request("/merchant_accounts?partner_id=#{order.partner_id}").first
+  end
+
+  def self.get_credit_card(order)
+    Adapters::GravityV1.request("/credit_card/#{order.credit_card_id}")
   end
 
   def self.can_submit?(order)
--- spec/services/payment_service_spec.rb
+++ spec/services/payment_service_spec.rb
@@ -2,17 +2,25 @@ require 'rails_helper'
 require 'support/gravity_helper'
 
 describe PaymentService, type: :services do
-  let(:credit_card_id) { 'cc-1' }
-  let(:merchant_account_id) { 'ma-1' }
+  let(:source_id) { 'cc-1' }
+  let(:destination_id) { 'ma-1' }
+  let(:customer_id) { 'cust-1' }
   let(:currency_code) { 'usd' }
   let(:charge_amount) { 2222 }
-  let(:authorize_charge_params) { { amount: charge_amount, currency: currency_code, description: 'Artsy', source: credit_card_id, destination: merchant_account_id, capture: false } }
+  let(:authorize_charge_params) { { amount: charge_amount, currency: currency_code, description: 'Artsy', source: source_id, customer: customer_id, destination: destination_id, capture: false } }
   let(:charge) { { id: 'ch_22' } }
 
   describe '#authorize_charge' do
     it "authorizes a charge on the user's credit card" do
       allow(Stripe::Charge).to receive(:create).with(authorize_charge_params).and_return(charge)
-      PaymentService.authorize_charge(credit_card_id, merchant_account_id, charge_amount, currency_code)
+      params = {
+        source_id: source_id,
+        customer_id: customer_id,
+        destination_id: destination_id,
+        currency_code: currency_code,
+        amount: charge_amount
+      }
+      PaymentService.authorize_charge(params)
       expect(Stripe::Charge).to have_received(:create).with(authorize_charge_params)
     end
   end
