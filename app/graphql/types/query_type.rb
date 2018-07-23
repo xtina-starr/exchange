018886a99cb04108108c06b7420268f7be280e4d
myk bilokonsky <mbilokonsky@gmail.com>
2018-07-23 15:31:17 -0400

    allow trusted users to perform graphql queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -40,7 +40,7 @@ class Types::QueryType < Types::BaseObject
 
   def validate_orders_params!(params)
     raise GraphQL::ExecutionError, 'requires one of userId or partnerId' unless params[:user_id].present? || params[:partner_id].present?
-    raise GraphQL::ExecutionError, 'Not permitted' if params[:user_id] && params[:user_id] != context[:current_user][:id]
+    raise GraphQL::ExecutionError, 'Not permitted' if params[:user_id] && params[:user_id] != context[:current_user][:id] || !context[:current_user][:roles].include?('trusted')
     raise GraphQL::ExecutionError, 'Not permitted' if params[:partner_id] && !context[:current_user][:partner_ids].include?(params[:partner_id])
   end
 end
