dca7b8c1a2eaaf7ef6ec19b734ffc47a395feb04
ashkan <ashkan@artsymail.com>
2018-07-25 15:37:21 -0400

    Switch to Faraday for calls

--- app/services/artwork_service.rb
+++ app/services/artwork_service.rb
@@ -1,6 +1,6 @@
 module ArtworkService
   PRIVATE_ARTWORK_FIELDS = %i[price_changed_by flags inventory publish_change_requested_by location deleted_by gene_count partner_gene_count dates external_id display_price_range confidential_notes created_at inventory_id published_changed_at publish_change_requested_at secondary_market deleted dimensions_string].freeze
-  ARTWORK_CACHE_EXPIRATION_IN_MINUTES = 10
+  ARTWORK_CACHE_EXPIRATION_IN_MINUTES = Rails.application.config_for(:gravity)['artwork_cache_in_minutes']
   def self.find(artwork_id)
     find!(artwork_id)
   rescue Adapters::GravityError
--- spec/lib/adapters/gravity_v1_spec.rb
+++ spec/lib/adapters/gravity_v1_spec.rb
@@ -3,7 +3,7 @@ require 'webmock/rspec'
 
 describe Adapters::GravityV1 do
   it 'raises Gravity Error when get 404' do
-    stub_request(:get, /artwork/).to_return(status: 404)
+    stub_request(:get, /artwork/).to_return(status: 404, body: { error: 'cannot find' }.to_json)
     expect do
       Adapters::GravityV1.request('artwork/2')
     end.to raise_error(Adapters::GravityError)
