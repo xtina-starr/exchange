4afef4ab9993d0ddcdbe308f01df0316693b7898
ashkan <ashkan@artsymail.com>
2018-07-26 14:17:07 -0400

    Code review comments

--- app/services/artwork_service.rb
+++ app/services/artwork_service.rb
@@ -1,6 +1,6 @@
 module ArtworkService
   PRIVATE_ARTWORK_FIELDS = %i[price_changed_by flags inventory publish_change_requested_by location deleted_by gene_count partner_gene_count dates external_id display_price_range confidential_notes created_at inventory_id published_changed_at publish_change_requested_at secondary_market deleted dimensions_string].freeze
-  ARTWORK_CACHE_EXPIRATION_IN_MINUTES = Rails.application.config_for(:gravity)['artwork_cache_in_minutes']
+
   def self.find(artwork_id)
     find!(artwork_id)
   rescue Adapters::GravityError
@@ -8,7 +8,7 @@ module ArtworkService
   end
 
   def self.find!(artwork_id)
-    Rails.cache.fetch("gravity_artwork_#{artwork_id}", expires_in: ARTWORK_CACHE_EXPIRATION_IN_MINUTES.minutes) do
+    Rails.cache.fetch("gravity_artwork_#{artwork_id}", expires_in: Rails.application.config_for(:gravity)['artwork_cache_in_seconds']) do
       Adapters::GravityV1.request("/artwork/#{artwork_id}?include_deleted=true")
     end
   end
--- app/services/gravity_service.rb
+++ app/services/gravity_service.rb
@@ -1,6 +1,6 @@
 module GravityService
   def self.fetch_partner(partner_id)
-    Rails.cache.fetch("gravity_partner_#{partner_id}", expire_in: Rails.application.config_for(:gravity)['partner_cache_in_minutes'].minutes) do
+    Rails.cache.fetch("gravity_partner_#{partner_id}", expire_in: Rails.application.config_for(:gravity)['partner_cache_in_seconds']) do
       Adapters::GravityV1.request("/partner/#{partner_id}/all")
     end
   end
