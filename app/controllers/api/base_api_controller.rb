a149a5068d6e81d86884ce0f2b2c99f1bc63dbce
Sarah Weir <sweir27@gmail.com>
2018-08-06 15:43:48 -0400

    fix csrf issue

--- app/controllers/api/base_api_controller.rb
+++ app/controllers/api/base_api_controller.rb
@@ -2,5 +2,6 @@ module Api
   class BaseApiController < ApplicationController
     skip_before_action :require_artsy_authentication
     before_action :authenticate_request!
+    protect_from_forgery with: :null_session
   end
 end
