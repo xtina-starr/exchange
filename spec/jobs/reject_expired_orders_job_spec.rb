5920e4ece21975ef3d6a645b7a545f861bbde2d9
Will Doenlen <wdoenlen@gmail.com>
2018-08-17 13:42:54 -0400

    WIP: Add tests for rejecting expired orders

--- spec/jobs/reject_expired_orders_job_spec.rb
+++ spec/jobs/reject_expired_orders_job_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe RejectExpiredOrdersJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe RejectExpiredOrdersJob, type: :job do
+
+  let(:order) { Fabricate(:order) }
+
+  describe '#perform' do
+    before do
+      ActiveJob::Base.queue_adapter = :test
+    end
+
+    it 'rejects an order if the order has passed expiration time and is still in the same state' do
+      order.update!(state_expires_at: Time.new - 1.second)
+      RejectExpiredOrdersJob.perform_now(order.id, Order::PENDING)
+      expect(order.state).to eq Order::REJECTED
+    end
+
+    it 'does not reject an order if the order is not in the same state' do
+    end
+
+    it 'does not reject an order if the order has not passed its expiration time' do
+    end
+  end
 end
