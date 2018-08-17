2494c480b8df2c1e8a6c7d00932334f09e1ecf56
Will Doenlen <wdoenlen@gmail.com>
2018-08-17 16:03:39 -0400

    Add proper tests for expire_order_job

--- spec/jobs/expire_order_job_spec.rb
+++ spec/jobs/expire_order_job_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'timecop'
 
 describe ExpireOrderJob, type: :job do
 
@@ -9,24 +10,45 @@ describe ExpireOrderJob, type: :job do
   end
   
   describe '#perform' do
+    before do
+      allow(OrderService).to receive(:abandon!)
+      allow(OrderService).to receive(:reject!)
+    end
+
     context 'with an expired order' do
-      it 'transitions a pending order to an abandoned' do
-        allow(OrderService).to receive(:abandon!).with(order)
-        order.update!(state_expires_at: Time.new - 1.second)
+      it 'transitions a pending order to abandoned' do
+        Timecop.freeze(order.state_expires_at + 1.second)
         ExpireOrderJob.perform_now(order.id, Order::PENDING)
         expect(OrderService).to have_received(:abandon!).with(order)
       end
-      it 'transitions a submitted order to rejected order' do
+      it 'transitions a submitted order to rejected' do
+        order.update!(state: Order::SUBMITTED)
+        Timecop.freeze(order.state_expires_at + 1.second)
+        ExpireOrderJob.perform_now(order.id, Order::SUBMITTED)
+        expect(OrderService).to have_received(:reject!).with(order)
       end
       it 'transitions an approved order to rejected' do
+        order.update!(state: Order::APPROVED)
+        Timecop.freeze(order.state_expires_at + 1.second)
+        ExpireOrderJob.perform_now(order.id, Order::APPROVED)
+        expect(OrderService).to have_received(:reject!).with(order)
       end
     end
-    context 'with an order in a different state' do
+    context 'with an order in a different state than when the job was made' do
       it 'does nothing' do
+        order.update!(state: Order::SUBMITTED)
+        Timecop.freeze(order.state_expires_at + 1.second)
+        ExpireOrderJob.perform_now(order.id, Order::PENDING)
+        expect(OrderService).to_not have_received(:abandon!)
+        expect(OrderService).to_not have_received(:reject!)
       end
     end
     context 'with an order in the same state before its expiration time' do
-      it 'does nothing'
+      it 'does nothing' do
+        ExpireOrderJob.perform_now(order.id, Order::PENDING)
+        expect(OrderService).to_not have_received(:abandon!)
+        expect(OrderService).to_not have_received(:reject!)
+      end
     end
   end
 end
