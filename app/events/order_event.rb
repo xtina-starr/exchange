71dfcd814e1b964ce80cea4341a070e0e814fadb
Barry Hoggard <barry@hoggard.org>
2018-08-21 01:03:59 -0400

    use UTC not Eastern time for state_expires_at

--- app/events/order_event.rb
+++ app/events/order_event.rb
@@ -13,32 +13,30 @@ class OrderEvent < Events::BaseEvent
   end
 
   def properties
-    Time.use_zone('Eastern Time (US & Canada)') do
-      {
-        code: @object.code,
-        created_at: @object.created_at,
-        currency_code: @object.currency_code,
-        items_total_cents: @object.items_total_cents,
-        shipping_total_cents: @object.shipping_total_cents,
-        tax_total_cents: @object.tax_total_cents,
-        buyer_total_cents: @object.buyer_total_cents,
-        commission_fee_cents: @object.commission_fee_cents,
-        transaction_fee_cents: @object.transaction_fee_cents,
-        seller_total_cents: @object.seller_total_cents,
-        line_items: @object.line_items.map { |li| line_item_detail(li) },
-        partner_id: @object.partner_id,
-        shipping_name: @object.shipping_name,
-        shipping_address_line1: @object.shipping_address_line1,
-        shipping_address_line2: @object.shipping_address_line2,
-        shipping_city: @object.shipping_city,
-        shipping_country: @object.shipping_country,
-        shipping_postal_code: @object.shipping_postal_code,
-        shipping_region: @object.shipping_region,
-        state: @object.state,
-        updated_at: @object.updated_at,
-        state_expires_at: @object.state_expires_at
-      }
-    end
+    {
+      code: @object.code,
+      created_at: @object.created_at,
+      currency_code: @object.currency_code,
+      items_total_cents: @object.items_total_cents,
+      shipping_total_cents: @object.shipping_total_cents,
+      tax_total_cents: @object.tax_total_cents,
+      buyer_total_cents: @object.buyer_total_cents,
+      commission_fee_cents: @object.commission_fee_cents,
+      transaction_fee_cents: @object.transaction_fee_cents,
+      seller_total_cents: @object.seller_total_cents,
+      line_items: @object.line_items.map { |li| line_item_detail(li) },
+      partner_id: @object.partner_id,
+      shipping_name: @object.shipping_name,
+      shipping_address_line1: @object.shipping_address_line1,
+      shipping_address_line2: @object.shipping_address_line2,
+      shipping_city: @object.shipping_city,
+      shipping_country: @object.shipping_country,
+      shipping_postal_code: @object.shipping_postal_code,
+      shipping_region: @object.shipping_region,
+      state: @object.state,
+      updated_at: @object.updated_at,
+      state_expires_at: @object.state_expires_at
+    }
   end
 
   def line_item_detail(line_item)
