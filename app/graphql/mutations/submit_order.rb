59ffff8fcf74290926f2289e9daa639a58dd8cd5
David Sheldrick <djsheldrick@gmail.com>
2018-08-24 14:00:42 +0100

    use new mutation style for submit_order

--- app/graphql/mutations/submit_order.rb
+++ app/graphql/mutations/submit_order.rb
@@ -3,17 +3,13 @@ class Mutations::SubmitOrder < Mutations::BaseMutation
 
   argument :id, ID, required: true
 
-  field :order, Types::OrderType, null: true
-  field :errors, [String], null: false
+  field :order_or_error, Mutations::OrderOrFailureUnionType, 'A union of success/failure', null: false
 
   def resolve(id:)
     order = Order.find(id)
     validate_buyer_request!(order)
-    {
-      order: OrderSubmitService.call!(order, by: context[:current_user]['id']),
-      errors: []
-    }
+    { order_or_error: { order: OrderSubmitService.call!(order, by: context[:current_user]['id']) } }
   rescue Errors::ApplicationError, Errors::PaymentError => e
-    { order: nil, errors: [e.message] }
+    { order_or_error: { error: Types::MutationErrorType.from_application(e) } }
   end
 end
