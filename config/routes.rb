b1cc342fee46d57d358049e45e528caf7f1feff0
ashkan <ashkan@artsymail.com>
2018-08-30 09:45:05 -0400

    Add Sidekiq Dashboard, update readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   mount ArtsyAuth::Engine => '/'
+  constraints ->(req) { ArtsyAuthHelper.new(req.session[:access_token]).admin? } do
+    mount Sidekiq::Web, at: '/admin/sidekiq'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     post '/graphql', to: 'graphql#execute'
---
+++ lib/artsy_auth_helper.rb
@@ -0,0 +1,22 @@
+class ArtsyAuthHelper
+  ADMIN_ROLE = 'sales_admin'.freeze
+  def initialize(jwt)
+    @jwt = jwt
+    @decoded_token = decode_token
+  end
+
+  def admin?
+    return false unless @decoded_token
+    @decoded_token['roles'].include? ADMIN_ROLE
+  end
+
+  def current_user
+    @decoded_token.merge(id: @decoded_token[:sub])
+  end
+
+  private
+
+  def decode_token
+    JWT.decode(@jwt, Rails.application.config_for(:jwt)['hmac_secret'], true, algorithm: 'HS256')[0].with_indifferent_access
+  end
+end
