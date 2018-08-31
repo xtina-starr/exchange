ff2ea9bdbb29fdd4ace160f4faa5d4c1cbc763e3
Will Doenlen <wdoenlen@gmail.com>
2018-08-31 12:30:48 -0400

    Add specs for RecordSalesTaxJob

---
+++ spec/jobs/record_sales_tax_job_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe RecordSalesTaxJob, type: :job do
+  let(:order) { Fabricate(:order, fulfillment_type: Order::SHIP, shipping_country: 'US', shipping_postal_code: '10013', shipping_region: 'NY', shipping_city: 'New York', shipping_address_line1: '401 Broadway', shipping_total_cents: 100) }
+  let(:shipping) do
+    {
+      country: order.shipping_country,
+      postal_code: order.shipping_postal_code,
+      region: order.shipping_region,
+      city: order.shipping_city,
+      address_line_1: order.shipping_address_line1
+    }
+  end
+  let!(:line_item) { Fabricate(:line_item, order: order) }
+  describe '#perform' do
+    it 'instantiates a new SalesTaxService and calls record_tax_collected' do
+      sales_tax_instance = double
+      expect(SalesTaxService).to receive(:new).with(line_item, order.fulfillment_type, shipping, order.shipping_total_cents).and_return(sales_tax_instance)
+      expect(sales_tax_instance).to receive(:record_tax_collected)
+      RecordSalesTaxJob.perform_now(line_item.id)
+    end
+  end
+end
