74e75e03d8473922c6889908789244475a78d800
David Sheldrick <djsheldrick@gmail.com>
2018-09-04 11:32:43 +0100

    add test for phone number requiredness

--- spec/controllers/api/requests/set_shipping_mutation_request_spec.rb
+++ spec/controllers/api/requests/set_shipping_mutation_request_spec.rb
@@ -126,6 +126,17 @@ describe Api::GraphqlController, type: :request do
         expect(order.tax_total_cents).to eq 232
       end
 
+      context 'without phone number' do
+        it 'fails' do
+          response = client.execute(
+            mutation,
+            set_shipping_input.deep_merge(input: { phoneNumber: nil })
+          )
+          expect(response.data.set_shipping.order_or_error).to respond_to(:error)
+          expect(response.data.set_shipping.order_or_error.error.description).to eq 'Phone number is required' 
+        end
+      end
+
       describe '#shipping_total_cents' do
         before do
           expect(Adapters::GravityV1).to receive(:get).twice.with('/artwork/a-1').and_return(artwork1)
@@ -141,7 +152,7 @@ describe Api::GraphqlController, type: :request do
             expect(order.reload.shipping_total_cents).to eq 0
           end
         end
-        context 'with SHIP as fulfillment type' do
+        context 'with SHIP as fulfillment type' do 
           context 'with international shipping' do
             it 'sets total shipping cents properly' do
               response = client.execute(mutation, set_shipping_input)
