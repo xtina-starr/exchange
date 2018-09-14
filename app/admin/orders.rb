0e8ce5de696db5b5527f4091c07a1aa25f2b8745
Anson <ansor4@gmail.com>
2018-09-14 15:51:52 -0400

    first pass

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -3,15 +3,15 @@ ActiveAdmin.register Order do
 
   scope('Active', default: true) { |scope| scope.active }
   scope('Pending') { |scope| scope.pending }
+  scope('Submitted') { |scope| scope.where(state: Order::SUBMITTED) }
 
-  filter :seller_id_eq, label: 'Partner Id'
+  filter :seller_id_eq, label: 'Seller Id'
   filter :buyer_id_eq, label: 'User Id'
   filter :fulfillment_type, as: :check_boxes, collection: proc { Order::FULFILLMENT_TYPES }
+  filter :state, as: :check_boxes, collection: proc { Order::STATES }
 
   index do
     column :id
-    column :seller_id
-    column :buyer_id
     column :state
     column :items_total_cents
     column :currency
