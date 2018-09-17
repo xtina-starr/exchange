1a198a6ec76dd903d340c1ac64f514a200946cd5
ashkan <ashkan@artsymail.com>
2018-09-17 11:23:13 -0400

    Addiing queue size

--- app/controllers/api/health_controller.rb
+++ app/controllers/api/health_controller.rb
@@ -4,9 +4,11 @@ module Api
     SIDEKIQ_ALERT_THRESHOLD = 100
 
     def index
+      sidekiq_default_queue_size = Sidekiq::Queue.new.size
       render json: {
-        healthy: true,
-        worker_health: Sidekiq::Queue.new.size < SIDEKIQ_ALERT_THRESHOLD ? 'OK' : 'OY'
+        api_healthy: true,
+        worker_health:  sidekiq_default_queue_size < SIDEKIQ_ALERT_THRESHOLD ? 'OK' : 'OY',
+        worker_default_queue_size: sidekiq_default_queue_size
       }
     end
   end
--- spec/controllers/api/health_controller_spec.rb
+++ spec/controllers/api/health_controller_spec.rb
@@ -6,7 +6,10 @@ describe Api::HealthController, type: :request do
       it 'returns healthy: true' do
         get '/api/health'
         expect(response.status).to eq 200
-        expect(response.body).to eq({ healthy: true }.to_json)
+        result = JSON.parse(response.body)
+        expect(result['api_healthy']).to eq(true)
+        expect(result['worker_health']).to eq('OK')
+        expect(result['worker_default_queue_size']).not_to be_nil
       end
     end
   end
