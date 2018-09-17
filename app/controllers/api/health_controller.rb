b8548b4e8d5fde84fe4099fcddcef0557aaf2a73
ashkan <ashkan@artsymail.com>
2018-09-17 11:08:34 -0400

    Add Sidekiq check to our health endpoint

--- app/controllers/api/health_controller.rb
+++ app/controllers/api/health_controller.rb
@@ -1,9 +1,13 @@
 module Api
   class HealthController < Api::BaseApiController
     skip_before_action :authenticate_request!
+    SIDEKIQ_ALERT_THRESHOLD = 100
 
     def index
-      render json: { healthy: true }
+      render json: {
+        healthy: true,
+        worker_health: Sidekiq::Queue.new.size < SIDEKIQ_ALERT_THRESHOLD ? 'OK' : 'OY'
+      }
     end
   end
 end
