1c66b3cb7ff960c13b9f9b5329daa0d60e717091
Jon Allured <jon.allured@gmail.com>
2018-09-20 14:38:57 -0500

    One more try at good descriptions
    Words are hard.

--- app/graphql/types/order_connection_sort_enum.rb
+++ app/graphql/types/order_connection_sort_enum.rb
@@ -2,8 +2,8 @@ class Types::OrderConnectionSortEnum < Types::BaseEnum
   description 'Fields to sort by'
   graphql_name 'OrderConnectionSortEnum'
 
-  value 'UPDATED_AT_ASC', 'Sort by timestamp order was last updated in ascending order'
-  value 'UPDATED_AT_DESC', 'Sort by timestamp order was last updated in descending order'
-  value 'STATE_UPDATED_AT_ASC', 'Sort by timestamp state of order was last updated in ascending order'
-  value 'STATE_UPDATED_AT_DESC', 'Sort by timestamp state of order was last updated in descending order'
+  value 'UPDATED_AT_ASC', 'Sort by the timestamp the order was last updated in ascending order'
+  value 'UPDATED_AT_DESC', 'Sort by the timestamp the order was last updated in descending order'
+  value 'STATE_UPDATED_AT_ASC', 'Sort by the timestamp the state of order was last updated in ascending order'
+  value 'STATE_UPDATED_AT_DESC', 'Sort by the timestamp the state of order was last updated in descending order'
 end
