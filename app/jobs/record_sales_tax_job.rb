ec02ce5853c9796ee1f81769e2d9dc0595f04b97
Will Doenlen <wdoenlen@gmail.com>
2018-09-25 12:47:27 -0400

    Use Address object in RecordSalesTaxJob and override Address equality operator to test for equal address attributes

--- app/jobs/record_sales_tax_job.rb
+++ app/jobs/record_sales_tax_job.rb
@@ -4,13 +4,14 @@ class RecordSalesTaxJob < ApplicationJob
   def perform(line_item_id)
     line_item = LineItem.find(line_item_id)
     artwork = GravityService.get_artwork(line_item.artwork_id)
-    shipping = {
+    artwork_address = Address.new(artwork[:location])
+    shipping_address = Address.new(
       country: line_item.order.shipping_country,
       postal_code: line_item.order.shipping_postal_code,
       region: line_item.order.shipping_region,
       city: line_item.order.shipping_city,
       address_line1: line_item.order.shipping_address_line1
-    }
-    SalesTaxService.new(line_item, line_item.order.fulfillment_type, shipping, line_item.order.shipping_total_cents, artwork[:location]).record_tax_collected
+    )
+    SalesTaxService.new(line_item, line_item.order.fulfillment_type, shipping_address, line_item.order.shipping_total_cents, artwork_address).record_tax_collected
   end
 end
--- spec/jobs/record_sales_tax_job_spec.rb
+++ spec/jobs/record_sales_tax_job_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 describe RecordSalesTaxJob, type: :job do
   let(:order) { Fabricate(:order, fulfillment_type: Order::SHIP, shipping_country: 'US', shipping_postal_code: '10013', shipping_region: 'NY', shipping_city: 'New York', shipping_address_line1: '401 Broadway', shipping_total_cents: 100) }
-  let(:shipping) do
-    {
+  let(:shipping_address) do
+    Address.new(
       country: order.shipping_country,
       postal_code: order.shipping_postal_code,
       region: order.shipping_region,
       city: order.shipping_city,
       address_line1: order.shipping_address_line1
-    }
+    )
   end
   let!(:line_item) { Fabricate(:line_item, order: order) }
-  let(:artwork_location) { gravity_v1_artwork[:location] }
+  let(:artwork_address) { Address.new(gravity_v1_artwork[:location]) }
   describe '#perform' do
     it 'instantiates a new SalesTaxService and calls record_tax_collected' do
       sales_tax_instance = double
-      expect(SalesTaxService).to receive(:new).with(line_item, order.fulfillment_type, shipping, order.shipping_total_cents, artwork_location).and_return(sales_tax_instance)
+      expect(SalesTaxService).to receive(:new).with(line_item, order.fulfillment_type, shipping_address, order.shipping_total_cents, artwork_address).and_return(sales_tax_instance)
       expect(sales_tax_instance).to receive(:record_tax_collected)
       expect(GravityService).to receive(:get_artwork).with(line_item.artwork_id).and_return(gravity_v1_artwork)
       RecordSalesTaxJob.perform_now(line_item.id)
