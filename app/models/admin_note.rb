95f48a023373b480ac90c30bb67d7e32c95f1ebb
Anson <ansor4@gmail.com>
2018-09-26 15:10:10 -0400

    update admin actions

--- app/models/admin_note.rb
+++ app/models/admin_note.rb
@@ -1,12 +1,14 @@
 class AdminNote < ApplicationRecord
   belongs_to :order
   TYPES = {
-    watching: 'watching'.freeze,
-    contacted_to_arrange_pickup: 'contacted_to_arrange_pickup'.freeze,
-    contacted_buyer: 'contacted_buyer'.freeze,
-    contacted_seller: 'contacted_seller'.freeze,
-    manual_order_change: 'manual_order_change'.freeze,
-    resolved_conversation: 'resolved_conversation'.freeze,
-    not_yet_contacted: 'not_yet_contacted'.freeze
+    case_opened_return: 'case_opened_return'.freeze,
+    case_opened_cancellation: 'case_opened_cancellation'.freeze,
+    mediation_contacted_buyer: 'mediation_contacted_buyer'.freeze,
+    mediation_contacted_seller: 'mediation_contacted_seller'.freeze,
+    case_agreement_reached: 'case_agreement_reached'.freeze,
+    execution_contacted_buyer: 'execution_contacted_buyer'.freeze,
+    execution_contacted_seller: 'execution_contacted_seller'.freeze,
+    execution_initiated_refund: 'execution_initiated_refund'.freeze,
+    case_closed: 'case_closed'.freeze,
   }.freeze
 end
