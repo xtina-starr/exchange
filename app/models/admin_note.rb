e62f14c21d45ca13e52210f05f9fb5f252a5f903
Anson <ansor4@gmail.com>
2018-09-26 17:24:52 -0400

    rubocopped

--- app/models/admin_note.rb
+++ app/models/admin_note.rb
@@ -9,6 +9,6 @@ class AdminNote < ApplicationRecord
     execution_contacted_buyer: 'execution_contacted_buyer'.freeze,
     execution_contacted_seller: 'execution_contacted_seller'.freeze,
     execution_initiated_refund: 'execution_initiated_refund'.freeze,
-    case_closed: 'case_closed'.freeze,
+    case_closed: 'case_closed'.freeze
   }.freeze
 end
