8c82f741e7e5dbcfca017942ed2495c58710f3f2
Ashkan Nasseri <ashkan@artsymail.com>
2018-09-27 09:39:23 -0400

    Code review comments

--- app/services/create_order_service.rb
+++ app/services/create_order_service.rb
@@ -74,7 +74,7 @@ class CreateOrderService
       # if there is one we are going to assume thats the one buyer meant to buy
       # TODO: ☝ is a temporary logic till Eigen starts supporting editionset artworks
       return unless @artwork[:edition_sets]&.count
-      raise Errors::ValidationError.new(:missing_edition_set_id, artwork_id: @artwork_id) if @artwork[:edition_sets].present? && @artwork[:edition_sets].count > 1
+      raise Errors::ValidationError.new(:missing_edition_set_id, artwork_id: @artwork_id) if @artwork[:edition_sets].count > 1
 
       @edition_set = @artwork[:edition_sets].first
       @edition_set_id = @edition_set[:id]
