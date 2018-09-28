41d3fac6527766e17c9585ed286e8ab78b79a6e1
ashkan <ashkan@artsymail.com>
2018-09-28 13:44:18 -0400

    Another pass

--- hokusai/ci.sh
+++ hokusai/ci.sh
@@ -5,5 +5,4 @@ set +x
 rails db:create RACK_ENV=test
 rails db:setup RACK_ENV=test
 rails db:migrate RACK_ENV=test
-echo $COVERALLS_REPO_TOKEN
-COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN bundle exec rake
+COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN bundle exec coveralls push
