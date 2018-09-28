e44f67172abbe72f49384e75f3c6c464e5ed98ee
ashkan <ashkan@artsymail.com>
2018-09-28 13:53:02 -0400

    Switch back to rake

--- hokusai/ci.sh
+++ hokusai/ci.sh
@@ -5,4 +5,4 @@ set +x
 rails db:create RACK_ENV=test
 rails db:setup RACK_ENV=test
 rails db:migrate RACK_ENV=test
-COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN bundle exec coveralls push
+COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN bundle exec rake
