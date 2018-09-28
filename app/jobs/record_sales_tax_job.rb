3b0571ffb74d73bb294e23dc91bd9d8a69da9621
Will Doenlen <wdoenlen@gmail.com>
2018-09-28 11:53:46 -0400

    Refactor RecordSalesTaxJob to skip job if Artsy should not remit taxes, use order.shipping_address and save sales tax transaction id if it's present

--- app/jobs/record_sales_tax_job.rb
+++ app/jobs/record_sales_tax_job.rb
@@ -3,17 +3,12 @@ class RecordSalesTaxJob < ApplicationJob
 
   def perform(line_item_id)
     line_item = LineItem.find(line_item_id)
+    return unless line_item.should_remit_sales_tax
+
     artwork = GravityService.get_artwork(line_item.artwork_id)
     artwork_address = Address.new(artwork[:location])
-    shipping_address = Address.new(
-      country: line_item.order.shipping_country,
-      postal_code: line_item.order.shipping_postal_code,
-      region: line_item.order.shipping_region,
-      city: line_item.order.shipping_city,
-      address_line1: line_item.order.shipping_address_line1
-    )
-    service = SalesTaxService.new(line_item, line_item.order.fulfillment_type, shipping_address, line_item.order.shipping_total_cents, artwork_address)
+    service = SalesTaxService.new(line_item, line_item.order.fulfillment_type, line_item.order.shipping_address, line_item.order.shipping_total_cents, artwork_address)
     service.record_tax_collected
-    line_item.update!(sales_tax_transaction_id: service.transaction.transaction_id)
+    line_item.update!(sales_tax_transaction_id: service.transaction.transaction_id) if service.transaction.present?
   end
 end
--- spec/jobs/record_sales_tax_job_spec.rb
+++ spec/jobs/record_sales_tax_job_spec.rb
@@ -3,7 +3,7 @@ require 'support/taxjar_helper'
 
 describe RecordSalesTaxJob, type: :job do
   let(:order) { Fabricate(:order, fulfillment_type: Order::PICKUP, shipping_country: 'US', shipping_postal_code: '10013', shipping_region: 'WA', shipping_city: 'New York', shipping_address_line1: '401 Broadway', shipping_total_cents: 100, state: Order::APPROVED) }
-  let!(:line_item) { Fabricate(:line_item, order: order, sales_tax_cents: 100_00) }
+  let!(:line_item) { Fabricate(:line_item, order: order, sales_tax_cents: 100_00, should_remit_sales_tax: true) }
   let(:artwork_location) do
     {
       country: 'US',
@@ -17,11 +17,23 @@ describe RecordSalesTaxJob, type: :job do
     before do
       stub_tax_for_order
     end
-
-    it 'posts a transaction to TaxJar and saves the transaction id' do
-      expect(GravityService).to receive(:get_artwork).with(line_item.artwork_id).and_return(gravity_v1_artwork(location: artwork_location))
-      RecordSalesTaxJob.perform_now(line_item.id)
-      expect(line_item.reload.sales_tax_transaction_id).to eq '123'
+    context 'with an order that has sales tax to remit' do
+      it 'posts a transaction to TaxJar and saves the transaction id' do
+        expect(GravityService).to receive(:get_artwork).with(line_item.artwork_id).and_return(gravity_v1_artwork(location: artwork_location))
+        expect(Address).to receive(:new).with(artwork_location).and_return(Address.new(artwork_location))
+        RecordSalesTaxJob.perform_now(line_item.id)
+        expect(line_item.reload.sales_tax_transaction_id).to eq '123'
+      end
+      context 'with an order that does not have sales tax to remit' do
+        before do
+          line_item.update!(should_remit_sales_tax: false)
+        end
+        it 'does nothing' do
+          expect(SalesTaxService).to_not receive(:new)
+          RecordSalesTaxJob.perform_now(line_item.id)
+          expect(line_item.reload.sales_tax_transaction_id).to be_nil
+        end
+      end
     end
   end
 end
