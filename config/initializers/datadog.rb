54fcc314b800648615cba4ea06ba48632c3b9ded
saolsen <steve.olsen@gmail.com>
2018-10-01 15:27:09 -0400

    let me turn on debugging

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,9 +3,10 @@
 Datadog.configure do |c|
   enabled = ENV.fetch('DATADOG_ENABLED', false)
   hostname = ENV.fetch('TRACE_AGENT_HOSTNAME', 'localhost')
+  debug = ENV.fetch('DATADOG_DEBUG', false)
 
-  c.tracer enabled: enabled, hostname: hostname, distributed_tracing: true, debug: false
-  c.use :rails, service_name: 'exchange', controller_service: 'exchange.controller', cache_service: 'exchange.cache', database_service: 'exchange.postgres'
+  c.tracer enabled: enabled, hostname: hostname, distributed_tracing: true, debug: debug
+  c.use :rails, service_name: 'exchange', distributed_tracing: true, controller_service: 'exchange.controller', cache_service: 'exchange.cache', database_service: 'exchange.postgres'
   c.use :redis, service_name: 'exchange.redis'
   c.use :sidekiq, service_name: 'exchange.sidekiq'
   c.use :http, service_name: 'exchange.http', distributed_tracing: true
