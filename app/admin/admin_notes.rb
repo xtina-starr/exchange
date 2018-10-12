0661325949db64c6a029b0c2483db1f6a662de62
Anson <ansor4@gmail.com>
2018-10-12 15:34:52 -0400

    whoops, remove this

--- app/admin/admin_notes.rb
+++ app/admin/admin_notes.rb
@@ -4,7 +4,6 @@ ActiveAdmin.register AdminNote do
 
   form do |f|
     f.inputs do
-      byebug
       f.input :order_id, as: :string, input_html: {readonly: true, value: order.id}
       f.input :admin_id, input_html: {readonly: true, value: current_user[:id]}
       f.input :note_type, :as => :select, :collection => AdminNote::TYPES.collect { |type| [type[1].humanize, type[0]] }
