47922a5228d2de5ef285dce0517078ee10b0abe0
Will Doenlen <wdoenlen@gmail.com>
2018-10-22 17:09:03 -0400

    Fetch partner locations for RecordSalesTaxJob and update specs

--- app/jobs/record_sales_tax_job.rb
+++ app/jobs/record_sales_tax_job.rb
@@ -7,7 +7,8 @@ class RecordSalesTaxJob < ApplicationJob
 
     artwork = GravityService.get_artwork(line_item.artwork_id)
     artwork_address = Address.new(artwork[:location])
-    service = SalesTaxService.new(line_item, line_item.order.fulfillment_type, line_item.order.shipping_address, line_item.order.shipping_total_cents, artwork_address)
+    seller_addresses = GravityService.fetch_partner_locations(line_item.order.seller_id)
+    service = SalesTaxService.new(line_item, line_item.order.fulfillment_type, line_item.order.shipping_address, line_item.order.shipping_total_cents, artwork_address, seller_addresses)
     service.record_tax_collected
     line_item.update!(sales_tax_transaction_id: service.transaction.transaction_id) if service.transaction.present?
   end
