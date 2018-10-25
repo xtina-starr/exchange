c5bb7cac7c7a4f89e17bb325b2dae485fbaad3e0
ashkan18 <ashkan18@gmail.com>
2018-10-25 06:38:36 -0400

    Code review comments

--- db/migrate/20181022140918_add_mode_to_orders.rb
+++ db/migrate/20181022140918_add_mode_to_orders.rb
@@ -1,8 +1,12 @@
 class AddModeToOrders < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :orders, :mode, :string, null: true
     add_index :orders, :mode
     Order.update_all(mode: Order::BUY)
     change_column :orders, :mode, :string, null: false
+  end
+
+  def down
+    remove_column :orders, :mode
   end
 end
