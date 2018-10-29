e63087ac68c114fc9a830d7aa0dc51f8bfefeb07
Sarah Weir <sweir27@gmail.com>
2018-10-29 17:51:42 -0400

    update offer order mutation

--- app/graphql/mutations/create_offer_order_with_artwork.rb
+++ app/graphql/mutations/create_offer_order_with_artwork.rb
@@ -5,13 +5,15 @@ class Mutations::CreateOfferOrderWithArtwork < Mutations::BaseMutation
   argument :edition_set_id, String, 'EditionSet Id', required: false
   argument :quantity, Integer, 'Number of items in the line item, default is 1', required: false
 
-  field :order_or_error, Mutations::OrderOrErrorUnionType, 'A union of Order/ApplicationError', null: false
+  field :order_or_error, Mutations::OrderOrFailureUnionType, 'A union of success/failure', null: false
 
   def resolve(artwork_id:, edition_set_id: nil, quantity: 1)
     service = CreateOfferOrderService.new(user_id: context[:current_user][:id], artwork_id: artwork_id, edition_set_id: edition_set_id, quantity: quantity)
     service.process!
-    { order_or_error: service.order }
+    {
+      order_or_error: { order: service.order }
+    }
   rescue Errors::ApplicationError => e
-    { order_or_error: e }
+    { order_or_error: { error: Types::ApplicationErrorType.from_application(e) } }
   end
 end
