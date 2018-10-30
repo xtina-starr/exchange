e8b4101ce75cce57c5d8f0f720318f94873237ef
Anson <ansor4@gmail.com>
2018-10-30 15:10:29 -0400

    PR changes, more asserts

--- spec/system/visiting_order_details_spec.rb
+++ spec/system/visiting_order_details_spec.rb
@@ -1,18 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'visit order details', type: :system do
-  let(:partner_id) { 'partner1' }
-  let(:user_id) { 'user1' }
-  let(:state) { Order::PENDING }
-  let(:created_at) { 2.days.ago }
   let!(:order) do
     Fabricate(
       :order,
-      seller_id: partner_id,
+      seller_id: 'partner1',
       seller_type: 'partner',
-      buyer_id: user_id,
+      buyer_id: 'user1',
       buyer_type: 'user',
-      created_at: created_at,
+      created_at: 2.days.ago,
       updated_at: 1.day.ago,
       shipping_total_cents: 100_00,
       commission_fee_cents: 50_00,
@@ -20,8 +16,8 @@ RSpec.describe 'visit order details', type: :system do
       seller_total_cents: 50_00,
       buyer_total_cents: 100_00,
       items_total_cents: 0,
-      state: state,
-      state_reason: state == Order::CANCELED ? 'seller_lapsed' : nil
+      state: Order::PENDING,
+      state_reason: nil
     )
   end
   before do
@@ -48,34 +44,15 @@ RSpec.describe 'visit order details', type: :system do
       .to_return(status: 200, body: {}.to_json, headers: {})
   end
 
-  it 'renders order overview page', js: true do
-    allow_any_instance_of(ApplicationController).to receive(:require_artsy_authentication)
-    visit '/admin'
-  end
-
-  it 'is able to switch to All Orders tab, where it shows an order', js: true do
-    order.save
-
-    allow_any_instance_of(ApplicationController).to receive(:require_artsy_authentication)
-    visit '/admin'
-
-    row_selector = 'td.col.col-code'
-
-    expect(page).to_not have_selector(row_selector)
-
-    tab_selector = 'li.scope.all'
-    within tab_selector do
-      click_link 'All'
-    end
-
-    expect(page).to have_selector(row_selector)
-  end
-
   it 'renders order details page', js: true do
     order.save
 
     allow_any_instance_of(ApplicationController).to receive(:require_artsy_authentication)
     visit '/admin'
+
+    page_title_selector = 'h2#page_title'
+
+    expect(page).to have_selector(page_title_selector)
 
     row_selector = 'td.col.col-code'
 
@@ -90,6 +67,12 @@ RSpec.describe 'visit order details', type: :system do
 
     within row_selector do
       click_link order.code
+    end
+
+    expect(page).to have_selector(page_title_selector)
+
+    within page_title_selector do
+      expect(page).to have_content("Order #{order.id}")
     end
   end
 end
