f5c0ce8ca072ce83ee611b471ba60e50847f40e1
Anson <ansor4@gmail.com>
2018-11-01 15:35:28 -0400

    ok other specs did not like that

--- spec/fabricators/order_fabricator.rb
+++ spec/fabricators/order_fabricator.rb
@@ -6,13 +6,6 @@ Fabricator(:order) do
   seller_id { sequence(:partner_id) { |i| "partner-id-#{i}" } }
   seller_type { 'gallery' }
   state { Order::PENDING }
-  state_reason { nil }
   items_total_cents { 0 }
   currency_code { 'USD' }
-  shipping_total_cents { 100_00 }
-  commission_fee_cents { 50_00 }
-  commission_rate { 0.10 }
-  seller_total_cents { 50_00 }
-  buyer_total_cents { 100_00 }
-  items_total_cents { 0 }
 end
