8874279634954e7f1652a0206e89fb77c380d345
ashkan <ashkan@artsymail.com>
2018-11-05 15:37:11 -0500

    Add order.mode to posted events

--- spec/events/transaction_event_spec.rb
+++ spec/events/transaction_event_spec.rb
@@ -76,6 +76,7 @@ describe TransactionEvent, type: :events do
     it 'returns correct properties for a submitted order' do
       order.submit!
       expect(event.properties[:order][:id]).to eq order.id
+      expect(event.properties[:order][:mode]).to eq Order::BUY
       expect(event.properties[:order][:code]).to eq order.code
       expect(event.properties[:order][:currency_code]).to eq 'USD'
       expect(event.properties[:order][:state]).to eq 'submitted'
