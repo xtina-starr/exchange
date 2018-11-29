131576ca2ab912828f1ff6f78511dd18abf0461b
Will Doenlen <wdoenlen@gmail.com>
2018-11-29 14:54:21 -0500

    First pass at adding new tests for seller accept offer mutation

--- app/graphql/mutations/seller_reject_offer.rb
+++ app/graphql/mutations/seller_reject_offer.rb
@@ -11,8 +11,7 @@ class Mutations::SellerRejectOffer < Mutations::BaseMutation
     order = offer.order
 
     authorize_seller_request!(order)
-
-    Offers::RejectOfferService.new(offer: offer, reject_reason: reject_reason).process!
+    Offers::AcceptOfferService.new(offer: offer, order: order).process!
 
     { order_or_error: { order: order.reload } }
   rescue Errors::ApplicationError => e
