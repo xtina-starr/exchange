44f41e18b9c702455d41b518b631526cf71ac09a
sepans <sepans@sepans.com>
2018-12-01 15:54:33 -0500

    Add mocks to seller_counter_offer_mutation spec

--- spec/controllers/api/requests/seller_counter_offer_mutation_request_spec.rb
+++ spec/controllers/api/requests/seller_counter_offer_mutation_request_spec.rb
@@ -6,9 +6,38 @@ describe Api::GraphqlController, type: :request do
     include_context 'GraphQL Client'
     let(:partner_id) { jwt_partner_ids.first }
     let(:user_id) { jwt_user_id }
-    let(:order) { Fabricate(:order, state: order_state, seller_id: partner_id, buyer_id: user_id) }
-    let(:offer) { Fabricate(:offer, order: order) }
+    let(:artwork_location) { { country: 'US' } }
+    let(:artwork) { { _id: 'a-1', current_version_id: '1', location: artwork_location, domestic_shipping_fee_cents: 1000 } }
     let(:order_state) { Order::SUBMITTED }
+    let!(:order) { Fabricate(:order, state: order_state, seller_id: partner_id, buyer_id: user_id, **shipping_info) }
+    let!(:offer) { Fabricate(:offer, order: order, amount_cents: 10000) }
+    let(:line_item_artwork_version) { artwork[:current_version_id] }
+    let!(:line_item) { Fabricate(:line_item, order: order, list_price_cents: 2000_00, artwork_id: artwork[:_id], artwork_version_id: line_item_artwork_version, quantity: 2) }
+    let(:partner_address) do
+      Address.new(
+        street_line1: '401 Broadway',
+        country: 'US',
+        city: 'New York',
+        region: 'NY',
+        postal_code: '10013'
+      )
+    end
+    let(:shipping_info) do
+      {
+        shipping_name: 'Fname Lname',
+        shipping_address_line1: '12 Vanak St',
+        shipping_address_line2: 'P 80',
+        shipping_city: 'New York',
+        shipping_postal_code: '02198',
+        buyer_phone_number: '00123456',
+        shipping_region: 'NY',
+        shipping_country: 'US',
+        fulfillment_type: Order::SHIP
+      }
+    end
+    let(:taxjar_client) { double }
+    let(:tax_response) { double(amount_to_collect: 3.00) }
+    let(:service) { Offers::CounterOfferService.new(offer: offer, amount_cents: 20000, from_type: Order::PARTNER) }
 
     let(:mutation) do
       <<-GRAPHQL
@@ -45,6 +74,11 @@ describe Api::GraphqlController, type: :request do
 
     before do
       order.update!(last_offer: offer)
+
+      allow(GravityService).to receive(:fetch_partner_locations).with(partner_id).and_return([partner_address])
+      allow(GravityService).to receive(:get_artwork).and_return(artwork)
+      allow(Taxjar::Client).to receive(:new).with(api_key: Rails.application.config_for(:taxjar)['taxjar_api_key'], api_url: nil).and_return(taxjar_client)
+      allow(taxjar_client).to receive(:tax_for_order).with(any_args).and_return(tax_response)
     end
 
     context 'when not in the submitted state' do
