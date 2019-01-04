8377a5102071c8ef130bf01ba4f2d3435f5942e1
Will Doenlen <wdoenlen@gmail.com>
2019-01-04 15:55:16 -0500

    Raise error if missing artwork location in ShippingHelper.calculate

--- lib/shipping_helper.rb
+++ lib/shipping_helper.rb
@@ -1,6 +1,7 @@
 class ShippingHelper
   def self.calculate(artwork, fulfillment_type, shipping_address = nil)
     return 0 if fulfillment_type == Order::PICKUP
+    raise Errors::ValidationError.new(:missing_artwork_location, artwork_id: artwork[:_id]) if artwork[:location].blank?
 
     if domestic?(artwork, shipping_address)
       artwork[:domestic_shipping_fee_cents] || raise(Errors::ValidationError, :missing_domestic_shipping_fee)
@@ -9,9 +10,7 @@ class ShippingHelper
     end
   end
 
-  private
-
   def self.domestic?(artwork, shipping_address)
-    artwork[:location][:country].casecmp(@shipping_address.country).zero? && (shipping_address.country != Carmen::Country.coded('US').code || shipping_address.continental_us?)
+    artwork[:location][:country].casecmp(shipping_address.country).zero? && (shipping_address.country != Carmen::Country.coded('US').code || shipping_address.continental_us?)
   end
 end
--- spec/lib/shipping_helper_spec.rb
+++ spec/lib/shipping_helper_spec.rb
@@ -44,14 +44,32 @@ describe ShippingHelper, type: :services do
       }
     }
   end
+  let(:missing_artwork_location_config) do
+    {
+      _id: 'a-3',
+      domestic_shipping_fee_cents: 100_00,
+      international_shipping_fee_cents: 500_00,
+      location: nil
+    }
+  end
 
   let(:artwork) { gravity_v1_artwork }
   let(:continental_us_artwork) { gravity_v1_artwork(continental_us_artwork_config) }
   let(:artwork_in_italy) { gravity_v1_artwork(artwork_in_italy_config) }
+  let(:artwork_missing_location) { gravity_v1_artwork(missing_artwork_location_config) }
   describe '#calculate' do
     context 'with pickup fulfillment type' do
       it 'returns 0' do
         expect(ShippingHelper.calculate(artwork, Order::PICKUP)).to eq 0
+      end
+    end
+    context 'with missing artowrk location' do
+      it 'raises an error' do
+        expect { ShippingHelper.calculate(artwork_missing_location, Order::SHIP, us_shipping) }.to raise_error do |error|
+          expect(error).to be_a(Errors::ValidationError)
+          expect(error.code).to eq :missing_artwork_location
+          expect(error.data[:artwork_id]).to eq artwork_missing_location[:_id]
+        end
       end
     end
     context 'continental us artwork' do
