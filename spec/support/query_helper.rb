dfb9b4686a380aba3f77b65340ed93e9a4844da1
Lily Pacegit <lilyfpace@gmail.com>
2019-06-06 09:40:21 -0400

    happy path tests for make offer

--- spec/support/query_helper.rb
+++ spec/support/query_helper.rb
@@ -151,4 +151,190 @@ module QueryHelper
       }
     }
   ).freeze
+
+  CREATE_OFFER_ORDER = %(
+    mutation($input: CreateOfferOrderWithArtworkInput!) {
+      createOfferOrderWithArtwork(input: $input) {
+        orderOrError {
+          ... on OrderWithMutationSuccess {
+            order {
+              id
+              mode
+              itemsTotalCents
+              totalListPriceCents
+              buyer {
+                ... on Partner {
+                  id
+                }
+              }
+              seller {
+                ... on User {
+                  id
+                }
+              }
+            }
+          }
+          ... on OrderWithMutationFailure {
+            error {
+              code
+              data
+              type
+            }
+          }
+        }
+      }
+    }
+  ).freeze
+
+  ADD_OFFER_TO_ORDER = %(
+    mutation($input: AddInitialOfferToOrderInput!) {
+      addInitialOfferToOrder(input: $input) {
+        orderOrError {
+          ... on OrderWithMutationSuccess {
+            order {
+              id
+              mode
+              totalListPriceCents
+              buyer {
+                ... on Partner {
+                  id
+                }
+              }
+              seller {
+                ... on User {
+                  id
+                }
+              }
+              ... on OfferOrder {
+                offers {
+                  edges {
+                    node {
+                      id
+                      amountCents
+                    }
+                  }
+                }
+                lastOffer {
+                  id
+                  amountCents
+                  from {
+                    ... on User {
+                      id
+                    }
+                  }
+                  creatorId
+                  respondsTo {
+                    id
+                  }
+                }
+
+                myLastOffer {
+                  id
+                  amountCents
+                  buyerTotalCents
+                  submittedAt
+                  creatorId
+                  note
+                  from {
+                    ... on User {
+                      id
+                    }
+                  }
+                  respondsTo {
+                    id
+                  }
+                }
+              }
+            }
+          }
+          ... on OrderWithMutationFailure {
+            error {
+              code
+              data
+              type
+            }
+          }
+        }
+      }
+    }
+  ).freeze
+
+  SUBMIT_PENDING_OFFER = %(
+    mutation($input: SubmitPendingOfferInput!) {
+      submitPendingOffer(input: $input) {
+        orderOrError {
+          ... on OrderWithMutationSuccess {
+            order {
+              id
+              state
+              ... on OfferOrder {
+                lastOffer {
+                  id
+                  submittedAt
+                }
+              }
+            }
+          }
+          ... on OrderWithMutationFailure {
+            error {
+              code
+              data
+              type
+            }
+          }
+        }
+      }
+    }
+  ).freeze
+
+  SELLER_ACCEPT_OFFER = %(
+    mutation($input: SellerAcceptOfferInput!) {
+      sellerAcceptOffer(input: $input) {
+        orderOrError {
+          ... on OrderWithMutationSuccess {
+            order {
+              id
+              state
+            }
+          }
+          ... on OrderWithMutationFailure {
+            error {
+              code
+              data
+              type
+            }
+          }
+        }
+      }
+    }
+  ).freeze
+
+  SUBMIT_ORDER_WITH_OFFER = %(
+    mutation($input: SubmitOrderWithOfferInput!) {
+      submitOrderWithOffer(input: $input) {
+        orderOrError {
+          ... on OrderWithMutationSuccess {
+            order {
+              id
+              state
+              ... on OfferOrder {
+                lastOffer {
+                  id
+                  submittedAt
+                }
+              }
+            }
+          }
+          ... on OrderWithMutationFailure {
+            error {
+              code
+              data
+              type
+            }
+          }
+        }
+      }
+    }
+  ).freeze
+
 end
