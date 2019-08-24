e85b03f2d2cf2dc8b4f7e3457ffd590efde6f828
ashkan <ashkan@artsymail.com>
2019-08-24 09:11:31 -0400

    more tests

--- spec/lib/offer_processor_spec.rb
+++ spec/lib/offer_processor_spec.rb
@@ -4,8 +4,8 @@ require 'support/gravity_helper'
 describe OfferProcessor, type: :services do
   include_context 'include stripe helper'
 
-  let(:order) { Fabricate(:order, seller_id: 'partner_1', buyer_id: 'user_1', state: Order::PENDING, mode: Order::OFFER) }
-  let(:offer) { Fabricate(:offer, amount_cents: 200, order: order) }
+  let(:order) { Fabricate(:order, seller_id: 'partner_1', seller_type: 'gallery', buyer_id: 'user_1', buyer_type: 'user', state: Order::PENDING, mode: Order::OFFER) }
+  let(:offer) { Fabricate(:offer, from_id: 'user_1', from_type: 'user', amount_cents: 200, order: order) }
   let(:op) { OfferProcessor.new(offer) }
 
   describe '#validate_offer!' do
@@ -64,14 +64,47 @@ describe OfferProcessor, type: :services do
   end
 
   describe '#confirm_payment_method!' do
+    it 'adds transaction to the order in case of success' do
+      transaction = Fabricate(:transaction, status: Transaction::SUCCESS)
+      expect(PaymentMethodService).to receive(:confirm_payment_method!).with(order).and_return(transaction)
+      expect { op.confirm_payment_method! }.to change(order.transactions, :count).by(1)
+      expect(order.transactions.first.id).to eq transaction.id
+    end
+    it 'adds transaction to the order and raises error in case of require action' do
+      transaction = Fabricate(:transaction, status: Transaction::REQUIRES_ACTION, payload: {client_secret: 'si_test1'})
+      expect(PaymentMethodService).to receive(:confirm_payment_method!).with(order).and_return(transaction)
+      expect { op.confirm_payment_method! }.to raise_error(Errors::PaymentRequiresActionError).and change(order.transactions, :count).by(1)
+      expect(order.transactions.first.id).to eq transaction.id
+    end
   end
 
   describe '#update_offer_submission_timestamp' do
+    it 'updates offers submitted at' do
+      op.update_offer_submission_timestamp
+      expect(offer.reload.submitted_at).not_to be_nil
+    end
   end
 
   describe '#on_success!' do
+    before do
+      order.update!(state: Order::SUBMITTED)
+      op.on_success
+    end
+    it 'queues OrderFollowUpJob' do
+      expect(OrderFollowUpJob).to have_been_enqueued.with(order.id, Order::SUBMITTED)
+    end
+    it 'queues OfferRespondReminderJob' do
+      expect(OfferRespondReminderJob).to have_been_enqueued.with(order.id, offer.id)
+    end
+    it 'posts offer event' do
+      expect(PostEventJob).to have_been_enqueued
+    end
   end
 
-  describe '#order_on_success!' do
+  describe '#order_on_success' do
+    it 'posts order event' do
+      op.order_on_success
+      expect(PostEventJob).to have_been_enqueued
+    end
   end
 end
