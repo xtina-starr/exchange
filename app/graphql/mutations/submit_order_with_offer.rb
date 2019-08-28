9ac5cd08bd0263a7d613a5458058c437af97ef66
ashkan <ashkan@artsymail.com>
2019-08-28 14:39:34 -0400

    Reuse setupintent

--- app/graphql/mutations/submit_order_with_offer.rb
+++ app/graphql/mutations/submit_order_with_offer.rb
@@ -2,14 +2,15 @@ class Mutations::SubmitOrderWithOffer < Mutations::BaseMutation
   null true
 
   argument :offer_id, ID, required: true
+  argument :confirmed_setup_intent_id, String, required: false
 
   field :order_or_error, Mutations::OrderOrFailureUnionType, 'A union of success/failure', null: false
 
-  def resolve(offer_id:)
+  def resolve(offer_id:, confirmed_setup_intent_id: nil)
     offer = Offer.find(offer_id)
     authorize_offer_owner_request!(offer)
 
-    OfferService.submit_order_with_offer(offer, current_user_id)
+    OfferService.submit_order_with_offer(offer, current_user_id, confirmed_setup_intent_id)
 
     { order_or_error: { order: offer.order } }
   rescue Errors::PaymentRequiresActionError => e
