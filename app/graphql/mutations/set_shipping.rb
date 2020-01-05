779dd4eb44ee8e76a8b7179e4c753e7e20addc89
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2020-01-05 17:36:24 -0500

    Remove byebug, update schema

--- app/graphql/mutations/set_shipping.rb
+++ app/graphql/mutations/set_shipping.rb
@@ -8,11 +8,11 @@ class Mutations::SetShipping < Mutations::BaseMutation
 
   field :order_or_error, Mutations::OrderOrFailureUnionType, 'A union of success/failure', null: false
 
-  def resolve(id:, fulfillment_type:, shipping: {}, phone_number: nil)
+  def resolve(id:, fulfillment_type:, shipping: nil, phone_number: nil)
     order = Order.find(id)
     authorize_buyer_request!(order)
-    # byebug
-    OrderService.set_shipping!(order, fulfillment_type: fulfillment_type, shipping: shipping, phone_number: phone_number || (shipping ? (shipping&.to_h[:phone_number]) : nil))
+
+    OrderService.set_shipping!(order, fulfillment_type: fulfillment_type, shipping: shipping, phone_number: phone_number || shipping&.phone_number)
     {
       order_or_error: { order: order }
     }
