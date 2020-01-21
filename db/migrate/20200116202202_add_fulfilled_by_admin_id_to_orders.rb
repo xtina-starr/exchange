f0e87b6b57fdd8997a84b709cf61a006da8eb220
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2020-01-21 11:55:16 -0500

    Add more tests

--- db/migrate/20200116202202_add_fulfilled_by_admin_id_to_orders.rb
+++ db/migrate/20200116202202_add_fulfilled_by_admin_id_to_orders.rb
@@ -1,4 +1,4 @@
-class AddFulfilledByAdminToFulfillments < ActiveRecord::Migration[6.0]
+class AddFulfilledByAdminIdToOrders < ActiveRecord::Migration[6.0]
   def change
     add_column :orders, :fulfilled_by_admin_id, :string
   end
