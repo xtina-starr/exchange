bfff10674c4e61713fd1e8766c208d7cc2d6c3af
Justin Bennett <zephraph@gmail.com>
2020-01-28 14:18:05 -0500

    Undo change to active_admin_comments

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 2020_01_24_213353) do
     t.uuid "resource_id"
     t.string "author_type"
     t.uuid "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
